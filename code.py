bb76dfdf3734332ff525a5791ed49130281048eb
karl Jaehnig <k.jaehnig@yahoo.com>
2018-11-06 14:36:12 -0600

    code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+def do_something():
+    Str1 = "One Fish... \n"
+    Str2 = "Two Fish... \n"
+    Str3 = "Red Fish... \n"
+    Str4 = "Blue Fish"
+    something = Str1+Str2+Str3+Str4
+    print(something)# this will make it much easier in future problems to see that something is actually happening
